f926774a3bbada81849ff2bd6046a9f174372651
Leonardo Wajda <dev.leowajda@tuta.io>
2024-12-12 16:57:44 +0100

    solution(ch_01): string compression

---
+++ java/src/main/java/cracking_the_coding_interview/ch_01/StringCompression.java
@@ -0,0 +1,23 @@
+package cracking_the_coding_interview.ch_01;
+
+public class StringCompression {
+
+    private static String compress(String s) {
+
+        StringBuilder sb = new StringBuilder();
+        int counter = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            counter++;
+
+            if (i + 1 >= s.length() || s.charAt(i + 1) != ch) {
+                sb.append(ch).append(counter);
+                counter = 0;
+            }
+        }
+
+        return (sb.length() < s.length()) ? sb.toString() : s;
+    }
+
+}
