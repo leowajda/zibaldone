9a8bb460724aa8be955bfcf4a3ded1c592f7e528
Leonardo Wajda <dev.leowajda@tuta.io>
2024-12-19 17:28:49 +0100

    chore(ch_02): add node class

---
+++ java/src/main/java/cracking_the_coding_interview/ch_02/Node.java
@@ -0,0 +1,12 @@
+package cracking_the_coding_interview.ch_02;
+
+public class Node {
+
+    int val;
+    Node next;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
