3dee7dad46ed87ab051f5e6bc315f95c2acf35b5
Leonardo Wajda <dev.leowajda@tuta.io>
2024-12-27 15:35:48 +0100

    solution(ch_03): stack min

---
+++ java/src/main/java/cracking_the_coding_interview/ch_03/StackMin.java
@@ -0,0 +1,33 @@
+package cracking_the_coding_interview.ch_03;
+
+import java.util.EmptyStackException;
+
+public class StackMin {
+
+    private IntStack stack;
+    private IntStack minStack;
+
+    public void push(int val) {
+        stack.push(val);
+        minStack.push(Math.min(val, min()));
+    }
+
+    public int min() {
+        return minStack.peek();
+    }
+
+    public int peek() {
+        return stack.peek();
+    }
+
+    public int pop() {
+        int val = stack.pop();
+        minStack.pop();
+        return val;
+    }
+
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+
+}
