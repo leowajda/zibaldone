263be99097598e54055b9bc449ad93a1cbbe896f
Leonardo Wajda <dev.leowajda@tuta.io>
2025-02-17 12:33:35 +0100

    chore(ch_04): add graph node class

---
+++ java/src/main/java/cracking_the_coding_interview/ch_04/GraphNode.java
@@ -0,0 +1,14 @@
+package cracking_the_coding_interview.ch_04;
+
+import java.util.Objects;
+
+public class GraphNode {
+
+    public int val;
+    public GraphNode[] children;
+
+    public GraphNode(int val) {
+        this.val = val;
+    }
+
+}
