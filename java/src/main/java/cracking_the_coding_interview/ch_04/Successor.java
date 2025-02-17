9d962dc5824b1b5173bd29313c6b3a5347248dba
Leonardo Wajda <dev.leowajda@tuta.io>
2025-02-17 13:32:35 +0100

    solution(ch_04): successor

---
+++ java/src/main/java/cracking_the_coding_interview/ch_04/Successor.java
@@ -0,0 +1,30 @@
+package cracking_the_coding_interview.ch_04;
+
+public class Successor {
+
+    private static TreeNode inOrderSuccessor(TreeNode node) {
+
+        if (node == null) return null;
+
+        if (node.right != null)
+            return bstMinimum(node.right);
+
+        TreeNode parent = node.parent, child = node;
+        while (parent != null && child == parent.right) {
+            child  = parent;
+            parent = parent.parent;
+        }
+
+        return parent;
+    }
+
+    private static TreeNode bstMinimum(TreeNode root) {
+        TreeNode node = root;
+        while (node.left != null)
+            node = node.left;
+        return node;
+    }
+
+
+
+}
