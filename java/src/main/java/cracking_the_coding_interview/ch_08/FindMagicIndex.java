a226083b6a7a78e2395eb6fa917a126eb1e63620
Leonardo Wajda <dev.leowajda@tuta.io>
2025-02-20 16:50:30 +0100

    solution(ch_08): magic index

---
+++ java/src/main/java/cracking_the_coding_interview/ch_08/FindMagicIndex.java
@@ -0,0 +1,21 @@
+package cracking_the_coding_interview.ch_08;
+
+public class FindMagicIndex {
+
+    private static int findMagicIndex(int[] nums) {
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    private static int helper(int[] nums, int left, int right) {
+        if (left > right) return -1;
+
+        int middle = left + (right - left) / 2;
+        if (nums[middle] == middle)
+            return middle;
+
+        int leftMagicIndex = helper(nums, left, middle - 1);
+        return leftMagicIndex != -1 ? leftMagicIndex : helper(nums, middle + 1, right);
+    }
+
+
+}
