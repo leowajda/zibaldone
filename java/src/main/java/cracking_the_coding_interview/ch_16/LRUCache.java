ddd38aca1df7d5609336981505952baeb3166b35
Leonardo Wajda <dev.leowajda@tuta.io>
2025-02-25 21:00:26 +0100

    solution(ch_16): lru cache

---
+++ java/src/main/java/cracking_the_coding_interview/ch_16/LRUCache.java
@@ -0,0 +1,88 @@
+package cracking_the_coding_interview.ch_16;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LRUCache {
+
+    private static class Node {
+        Node prev, next;
+        Integer key, value;
+
+        public Node(Integer key, Integer value) {
+            this.value = value;
+            this.key = key;
+        }
+
+        public void link(Node prev, Node next) {
+            this.prev = prev;
+            this.next = next;
+            prev.next = this;
+            next.prev = this;
+        }
+
+        public void unlink() {
+            Node prev = this.prev;
+            Node next = this.next;
+
+            this.prev = null;
+            this.next = null;
+
+            prev.next = next;
+            next.prev = prev;
+        }
+
+    }
+
+    private final Map<Integer, Node> lruCache;
+    private final Node leastUsed;
+    private final Node mostUsed;
+    private final int maxSize;
+    private int currSize;
+
+    public LRUCache(int maxSize) {
+        this.lruCache   = new HashMap<>(maxSize);
+        this.maxSize    = maxSize;
+        this.currSize   = 0;
+        this.leastUsed  = new Node(-1, -1);
+        this.mostUsed   = new Node(-1, -1);
+        leastUsed.next  = mostUsed;
+        mostUsed.prev   = leastUsed;
+    }
+
+    private void addEntry(Integer key, Integer value) {
+        var entry = new Node(key, value);
+        entry.link(mostUsed.prev, mostUsed);
+        lruCache.put(key, entry);
+    }
+
+    public int get(int key) {
+        if (!lruCache.containsKey(key)) return -1;
+
+        var entry = lruCache.get(key);
+        entry.unlink();
+        entry.link(mostUsed.prev, mostUsed);
+        return entry.value;
+    }
+
+    public void put(int key, int value) {
+        if (currSize < maxSize && !lruCache.containsKey(key)) {
+            addEntry(key, value);
+            currSize++;
+            return;
+        }
+
+        if (currSize <= maxSize && lruCache.containsKey(key)) {
+            var prevEntry = lruCache.get(key);
+            prevEntry.value = value;
+            prevEntry.unlink();
+            prevEntry.link(mostUsed.prev, mostUsed);
+            return;
+        }
+
+        var leastRecentlyUsed = leastUsed.next;
+        leastRecentlyUsed.unlink();
+        lruCache.remove(leastRecentlyUsed.key);
+        addEntry(key, value);
+    }
+}
