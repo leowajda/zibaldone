8b3d4157d18fb6b8d65fa6ccb270286a83d59308
Leonardo Wajda <dev.leowajda@tuta.io>
2025-03-01 13:05:58 +0100

    solution(ch_17): bi-node

---
+++ java/src/main/java/cracking_the_coding_interview/ch_17/Binode.java
@@ -0,0 +1,36 @@
+package cracking_the_coding_interview.ch_17;
+
+public class Binode {
+
+    Binode left, right;
+    int data;
+
+    public Binode(Binode left, Binode right, int data) {
+        this.left = left;
+        this.right = right;
+        this.data = data;
+    }
+
+    private static Binode prev;
+
+    private static Binode convert(Binode root) {
+        var dummy = new Binode(null, null, -1);
+        prev = dummy;
+        helper(root);
+        return dummy.right;
+    }
+
+    private static void helper(Binode root) {
+        if (root == null)
+            return;
+
+        helper(root.left);
+        prev.right = root;
+        prev       = root;
+        root.left  = null;
+        helper(root.right);
+    }
+
+
+
+}
