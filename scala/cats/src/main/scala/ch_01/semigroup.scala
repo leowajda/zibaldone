05f4793d7ba254d39bd871d9297cfda2d8e56bc9
Leonardo Wajda <dev.leowajda@tuta.io>
2025-03-20 16:45:04 +0100

    lesson(ch_01): semigroup

---
+++ scala/cats/src/main/scala/ch_01/semigroup.scala
@@ -0,0 +1,16 @@
+package com.zibaldone.cats
+package ch_01
+
+import cats.Semigroup
+import cats.instances.int.*
+import cats.syntax.semigroup.*
+
+extension [T: Semigroup](iterable: Iterable[T])
+  // ex. use extension method
+  def reduceIterable: T = iterable.reduce(_ |+| _)
+
+final case class Expense(id: Long, amount: Double)
+
+object Expense:
+  // ex. support new type
+  given Semigroup[Expense] = Semigroup.instance { (a, b) => Expense(a.id |+| b.id, a.amount |+| b.amount) }
