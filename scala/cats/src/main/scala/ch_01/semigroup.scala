51e87c492f9552d68860a0100e240c0d49783bbf
Leonardo Wajda <dev.leowajda@tuta.io>
2025-03-20 17:18:40 +0100

    lesson(ch_01): monoid

--- scala/cats/src/main/scala/ch_01/semigroup.scala
+++ scala/cats/src/main/scala/ch_01/semigroup.scala
@@ -5,6 +5,7 @@ import cats.Semigroup
 import cats.instances.int.*
 import cats.syntax.semigroup.*
 
+// will throw an exception on empty iterables
 extension [T: Semigroup](iterable: Iterable[T])
   // ex. use extension method
   def reduceIterable: T = iterable.reduce(_ |+| _)
