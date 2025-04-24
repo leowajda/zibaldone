4cd0937019c37cd9317bc96b67315682f1d7966e
Leonardo Wajda <dev.leowajda@tuta.io>
2025-04-24 18:56:39 +0200

    refactor: minor changes

--- scala/cats/src/main/scala/hierarchy/Monad.scala
+++ scala/cats/src/main/scala/hierarchy/Monad.scala
@@ -3,5 +3,5 @@ package hierarchy
 
 trait Monad[F[_]] extends Applicative[F] with FlatMap[F]:
 
-  final override def map[A, B](fa: F[A])(f: A => B): F[B]         = flatMap(fa)(a => pure(f(a)))
-  final override def product[A, B](fa: F[A], fb: F[B]): F[(A, B)] = flatMap(fa)(a => map(fb)(b => (a, b)))
+  override def map[A, B](fa: F[A])(f: A => B): F[B]         = flatMap(fa)(a => pure(f(a)))
+  override def product[A, B](fa: F[A], fb: F[B]): F[(A, B)] = flatMap(fa)(a => map(fb)(b => (a, b)))
