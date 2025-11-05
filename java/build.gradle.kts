b5661c6a975a71dab59a9f72206ce217f2533f4b
Leonardo Wajda <dev.leowajda@tuta.io>
2025-11-05 19:43:46 +0100

    build: multi-module project

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -5,15 +5,8 @@ plugins {
 group = "com.zibaldone"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
+subprojects {
+    repositories {
+        mavenCentral()
+    }
 }
--- java/settings.gradle.kts
+++ java/settings.gradle.kts
@@ -1,2 +1,4 @@
 rootProject.name = "zibaldone-java"
 
+include("cracking-the-coding-interview")
+include("spring-boot")
