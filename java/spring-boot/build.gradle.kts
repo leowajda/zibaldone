eabff7f4f9de307479708e23e59b75d2f2329c03
leowajda <dev.leowajda@tuta.io>
2026-04-11 14:07:06 +0200

    chore(spring-boot): refresh build and shared config

--- java/spring-boot/build.gradle.kts
+++ java/spring-boot/build.gradle.kts
@@ -30,10 +30,65 @@ repositories {
     }
 }
 
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.shell:spring-shell-dependencies:3.0.2")
+    }
+}
+
+
 dependencies {
+    // TODO: annotate what dependency is used where
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    // implementation("org.springframework.boot:spring-boot-starter-jdbc") already included in spring-boot-starter-data-jpa
+    implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.9.0")
+    runtimeOnly("com.h2database:h2")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    implementation("org.springframework.retry:spring-retry")
+    implementation("org.springframework:spring-aspects")
+
+    implementation("org.springframework.data:spring-data-commons")
+
+    implementation("org.springframework.boot:spring-boot-starter-json")
+
+    implementation("org.springframework.shell:spring-shell-starter")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // implementation("org.springframework.boot:spring-boot-starter") already included in spring-boot-starter-web
     implementation("org.springframework.boot:spring-boot-starter-web")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    /*
+
+    TODO FIX DEPENDENCIES NOT LOADING + TESTCONTAINER EXAMPLE
+    testImplementation("org.postgresql:postgresql")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:postgresql")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+
+    */
+
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    implementation("org.mapstruct:mapstruct:1.5.5.Final")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("com.nimbusds:nimbus-jose-jwt:9.37.3")
+    implementation("org.springframework.security:spring-security-oauth2-jose")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+
+
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 }
 
 
@@ -41,17 +96,22 @@ tasks {
     test {
         useJUnitPlatform()
     }
+
+    bootRun {
+        mainClass.set("com.tutego.ch_02.classpathScanning.Date4uApplication")
+    }
+
     withType<JavaCompile> {
         options.encoding = "UTF-8"
     }
-    withType<Javadoc> {
-        options.encoding = "UTF-8"
+
+    withType<JavaCompile> {
+        options.annotationProcessorPath = configurations.annotationProcessor.get()
     }
+
     named<BootBuildImage>("bootBuildImage") {
         imageName.set("tutego.date4u")
     }
-}
 
-springBoot {
-    buildInfo()
+
 }
